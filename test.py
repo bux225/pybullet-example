d053143525a750e33ea20d45be857ff1619cdf4f
Matthew Soltis <soltis.mg@gmail.com>
2021-10-11 18:11:03 -0600

    update

--- test.py
+++ test.py
@@ -4,15 +4,16 @@ import pybullet_data
 physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
 p.setGravity(0,0,-10)
-planeId = p.loadURDF("plane.urdf")
-startPos = [0,0,1]
+strut = p.createCollisionShape(p.GEOM_CYLINDER, radius=0.1, height=2)
+#planeId = p.loadURDF("plane.urdf")
+startPos = [0,0,5]
 startOrientation = p.getQuaternionFromEuler([0,0,0])
-boxId = p.loadURDF("r2d2.urdf",startPos, startOrientation)
+#boxId = p.loadURDF("r2d2.urdf",startPos, startOrientation)
 #set the center of mass frame (loadURDF sets base link frame) startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
 for i in range (10000):
     p.stepSimulation()
     time.sleep(1./240.)
-cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
+cubePos, cubeOrn = p.getBasePositionAndOrientation(strut)
 print(cubePos,cubeOrn)
 p.disconnect()
 
